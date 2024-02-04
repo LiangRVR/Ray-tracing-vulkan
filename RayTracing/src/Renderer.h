f99b08923d18a7759e5ecb42448b2f161fd5ef5b
LiangRVR <liangvillarrubia95@gmail.com>
2024-02-04 02:32:55 -0500

    Refactor ray tracing algorithm in Renderer.cpp

--- RayTracing/src/Renderer.h
+++ RayTracing/src/Renderer.h
@@ -19,9 +19,20 @@ public:
     std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; };
 
 private:
-    //glm::vec4 PerPixel();
+    struct HitPayload
+    {
+        float HitDistance;
+        glm::vec3 WordNormal;
+        glm::vec3 WorldPosition;
 
-    glm::vec4 TraceRay(const Scene &scene, const Ray &ray);
+        int ObjectIndex;
+    };
+
+    glm::vec4 PerPixel(uint32_t x, uint32_t y);
+
+    HitPayload TraceRay(const Ray &ray);
+    HitPayload ClosestHit(const Ray &ray, float hitDistance, int objectIndex);
+    HitPayload Miss(const Ray &ray);
 
 private:
     std::shared_ptr<Walnut::Image> m_FinalImage;
